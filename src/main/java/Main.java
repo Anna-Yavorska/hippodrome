6481c3c56b2f88d92a46967c309fba257ca8d9d2
Anna-Yavorska <aniayavorsk@gmail.com>
2024-12-19 18:54:40 +0100

    Add logging properties, and Logger in files Hippodrome, Horse, Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
         List<Horse> horses = List.of(
                 new Horse("Bucephalus", 2.4),
                 new Horse("Ace of Spades", 2.5),
@@ -13,6 +18,10 @@ public class Main {
                 new Horse("Pegasus", 2.9),
                 new Horse("Cherry", 3)
         );
+
+        String loggerStart = String.format("Start of the race. Number of participants: %d", horses.size());
+        logger.info(loggerStart);
+
         Hippodrome hippodrome = new Hippodrome(horses);
 
         for (int i = 0; i < 100; i++) {
@@ -23,6 +32,8 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
+
+        logger.info("End of the race. Winner: " + winnerName);
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
